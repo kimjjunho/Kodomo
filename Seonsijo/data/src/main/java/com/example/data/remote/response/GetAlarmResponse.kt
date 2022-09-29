9d4824a4b0cedca703248b5b801148a5d9748a3a
kimjjunho <kimjjun1007@gmail.com>
2022-09-29 13:06:49 +0900

    get alarm response -> entity list mapping 추가

--- Seonsijo/data/src/main/java/com/example/data/remote/response/GetAlarmResponse.kt
+++ Seonsijo/data/src/main/java/com/example/data/remote/response/GetAlarmResponse.kt
@@ -1,12 +1,27 @@
 package com.example.data.remote.response
 
 import com.example.domain.entity.alarm.GetAlarmEntity
-import com.example.domain.entity.alarm.GetAlarmEntity.AlarmData
 
 data class GetAlarmResponse(
     val alarm_list: List<AlarmData>
-)
+    ){
+    data class AlarmData(
+        val alarm_id: Int,
+        val target_date: String,
+        val index: Int,
+        val subject: String,
+        val content: String
+    )
+}
 
 fun GetAlarmResponse.toEntity() = GetAlarmEntity(
-    alarm_list = alarm_list
+    alarm_list = alarm_list.map { it.toList() }
+)
+
+fun GetAlarmResponse.AlarmData.toList() = GetAlarmEntity.AlarmData(
+    alarm_id = alarm_id,
+    target_date = target_date,
+    index = index,
+    subject = subject,
+    content = content
 )
